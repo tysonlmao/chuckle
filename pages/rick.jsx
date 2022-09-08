8952d7fa1bd597710b365e3c98b756404b828e0f
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-09-09 09:43:21 +1000

    Added a rick roll link to the app
    Using a refresh meta tag to point towards the content of a youtube link

---
+++ pages/rick.jsx
@@ -0,0 +1,19 @@
+import head from "next/head"
+import react from "react"
+import Head from "next/head"
+
+export default function rick() {
+    return (
+        <div>
+            <Head>
+                <title>got you</title>
+                <meta name="description" content="not a rickroll, i swear" />
+                <meta httpEquiv="Refresh" content="0; url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'" />
+            </Head>
+
+        <main>
+            <p>never gonna give you up</p>
+        </main>
+    </div>
+    )
+}
