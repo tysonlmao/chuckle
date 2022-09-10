4970d8d820821e6744bad98827aad150a4abbfb6
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-09-10 17:20:12 +1000

    Added dogecoin tipping

---
+++ pages/dogecoin.jsx
@@ -0,0 +1,17 @@
+import react from 'react'
+import Head from 'next/head'
+
+export default function Dogecoin() {
+    return(
+        <div className="">
+            <Head>
+                <title>doge</title>
+                <meta name="description" content="Send us a dogecoin tip!" />
+            </Head>
+
+            <div className="w3-display-middle">
+                <h1>go away</h1>
+            </div>
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -9,16 +9,20 @@ export default function Home() {
         <title>whale</title>
         <meta name="description" content="not whales ;)" />
       </Head>
-
-      <main>
-        <header>
-          <div className="w3-display-middle">
+      <header>
+        
+      </header>
+        <main>
+          <div className="w3-display-middle smell">
             <h1>WELCOME TO <a href="https://chuckle.city">CHUCKLE.CITY</a></h1>
-            <h2>BEGIN LEARNING ABOUT WHALES</h2>
-            <h4 className="w3-red">disclaimer: you probably wont learn much about whales</h4>
+            <h2 className="w3-center">BEGIN LEARNING ABOUT WHALES</h2>
+            <h4 className="w3-red w3-center">disclaimer: you probably wont learn much about whale</h4>
           </div>
-        </header>
-      </main>
+        </main>
+
+        <footer>
+          <p className="w3-center">We accept <a href="dogecoin">dogecoin</a> and <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Turkish Lira!</a></p>
+        </footer>
     </div> 
   )
 }
