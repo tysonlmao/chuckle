3ea5acf555d5b4ad69b508d45b945def03b19e01
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-09-14 22:02:25 +1000

    yes

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,6 +29,7 @@ export default function Home() {
           <footer className="footer-index">
             <p className="w3-center">We accept <a href="dogecoin" className="w3-text-red ">Dogecoin</a> and <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="w3-text-red">Turkish Lira!</a></p>
           </footer>
+          <Header/>
       </div>
 
     </div> 
