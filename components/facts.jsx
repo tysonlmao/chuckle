c1342e41ed7d855a9340788d4cfd2d05cf45ca77
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-09-22 21:29:23 +1000

    Update facts.jsx

--- components/facts.jsx
+++ components/facts.jsx
@@ -5,8 +5,8 @@ import Script from 'next/script'
 export default function Facts() {
     return(
         <div>
-            <Script src="https://scripts.chuckle.city/whaleFacts.js"></Script>
-            <marquee width="100%" direction="right" className="whale-facts">replace me</marquee>
+            <script src="https://scripts.chuckle.city/whaleFacts.js"></script>
+            <marquee width="100%" direction="right" id="whale-facts">replace me</marquee>
         </div>
     )
 }
