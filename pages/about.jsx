d79a3b29919cda9a808a9652ae6d83bed648840a
Tyson <83587481+tysonlmao@users.noreply.github.com>
2022-09-24 22:51:17 +1000

    started work on about page

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import Head from 'next/head';
+
+export default function About() {
+    return(
+        <div>
+        <Head>
+            <title>about us</title>
+        </Head>
+
+        <img src="../public/IMG_01.jpg"></img>
+        
+        </div>
+    )
+}
